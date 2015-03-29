fcd23ad73700eee02f6dd9550ad4553692ff4e80
Alex Gu <iarousecom@gmail.com>
2015-03-29 04:44:04 -0400

    Update v1.1

--- landing/scripts/main.js
+++ landing/scripts/main.js
@@ -1,22 +1,38 @@
 (function(){
     "use strict";
+    var App;
 
-    var $win, $header;
-    var date = new Date();
-    var year = date.getFullYear();
-
-    $("#copyright").text(year);
-    $(".navbar-nav a").smoothScroll();
-    $(".footer-menu a").smoothScroll();
-
-    $win = $(window);
-    $header = $('#header')
-    $win.on('scroll', function() {
-        if ($win.scrollTop() > 280) {
-            $header.addClass('navbar-fixed-top');
-        } else {
-            $header.removeClass('navbar-fixed-top');
+    App = {
+        init: function() {
+            App.setCopyRight();
+            // App.setFixedNavbar();
+            App.smoothScroll();
+        },
+        setCopyRight: function() {
+            var date, year;
+            date = new Date();
+            year = date.getFullYear();
+            $("#copyright").text(year);
+        },
+        smoothScroll: function() {
+            $(".navbar-nav a").smoothScroll();
+            $(".footer-menu a").smoothScroll();
+        },
+        setFixedNavbar: function() {
+            var $win, $header;
+            var h = 280;
+            $win = $(window);
+            $header = $('#header')
+            $win.on('scroll', function() {
+                if ($win.scrollTop() > h) {
+                    $header.addClass('navbar-fixed-top');
+                } else {
+                    $header.removeClass('navbar-fixed-top');
+                }
+            })
         }
-    })
+    }
+
+    App.init();
 
 }).call(this);
